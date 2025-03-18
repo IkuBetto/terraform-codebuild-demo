ddbb1fb3f82b8970c99be07cde80ec69ec9cbc6f
EC2 Default User <ec2-user@ip-172-31-12-132.ec2.internal>
2025-03-18 11:21:32 +0000

    Add Terraform configuration

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"  # 任意のリージョンに変更可能
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "terraform-codebuild-demo-${random_string.bucket_suffix.result}"
+}
+
+resource "random_string" "bucket_suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.demo_bucket.bucket
+}
